88399523483c2b18bac42ad16ecec770b1f68743
Douglas Goodall <doug@goodall.com>
2025-02-09 04:35:03 -0800

    add doc to xmlrpc_client.cpp

--- xmlrpc_client.cpp
+++ xmlrpc_client.cpp
@@ -1,3 +1,9 @@
+/////////////////////////////////////////////////////////////////////
+// /home/doug/public_html/fw/xmlrpc_client.cpp 2025-02-09 04:27    //
+// Derived from xmlrpc-c/examples/cpp/xmlrpc_sample_add_client.cpp //
+// Copyright (c) 2025 Douglas Wade Goodall. All Rights Reserved.   //
+/////////////////////////////////////////////////////////////////////
+
 #include <cstdlib>
 #include <string>
 #include <iostream>
@@ -7,6 +13,22 @@ using namespace std;
 #include <xmlrpc-c/girerr.hpp>
 #include <xmlrpc-c/base.hpp>
 #include <xmlrpc-c/client_simple.hpp>
+
+/****************************************************************************
+ * @brief The main entry point for the program.
+ *
+ * This function establishes a connection with an XML-RPC server
+ * and invokes a remote method to compute the sum of two integers.
+ * It also handles argument validation and error reporting. Note
+ * that this program does not accept any command-line arguments,
+ * and a message is displayed if arguments are provided.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv The array of command-line arguments (unused in this function).
+ * @return int Returns 0 on successful completion, or exits with
+ *         an error status if invalid arguments are supplied or
+ *         exceptions occur during execution.
+ ***************************************************************************/
 
 int
 main(int argc, char **) {
