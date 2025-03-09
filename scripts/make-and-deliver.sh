e42ef17b2b02b3464c4c72b8c5fa69b300fa2f5e
Douglas Goodall <doug@goodall.com>
2025-03-09 08:10:04 -0700

    add code to remove shared regions to make and build

--- scripts/make-and-deliver.sh
+++ scripts/make-and-deliver.sh
@@ -14,3 +14,8 @@ cp -v libmwfw*.a        ~/public_html/fw/lib/
 rm -f                   ~/public_html/fw/bin/vpad
 cp -v vpad*             ~/public_html/fw/bin/
 cp -v xmlrpc*           ~/public_html/fw/bin/
+ipcrm --shmem-key 0x00005164 2> /tmp/ipcrm#5164.stderr # MultiWare Main ShMem
+ipcrm --shmem-key 0x00005165 2> /tmp/ipcrm#5165.stderr # vpad control/status
+ipcrm --shmem-key 0x00005167 2> /tmp/ipcrm#5167.stderr # admin  user descriptor
+ipcrm --shmem-key 0x00005168 2> /tmp/ipcrm#5168.stderr # doug   user descriptor
+ipcrm --shmem-key 0x00005169 2> /tmp/ipcrm#5169.stderr # marcus user descriptor
