b6744271083a317fe1fd4ac7c2b88c3472c97a83
Douglas <doug@goodall.com>
2025-03-08 17:50:09 -0800

    bump version to 1.2.0.0

--- version.h
+++ version.h
@@ -6,12 +6,12 @@
 #ifndef VERSION_H
 #define VERSION_H
 #define RMAJ_VERSION          1
-#define RMIN_VERSION          0
+#define RMIN_VERSION          2
 #define RREV_VERSION           0
 #define RBUILD_VERSION         0
-#define RVERSION_STRING        "1.0.0.0"
-#define RVERSION_STRING_SHORT  "1.0.0"
-#define RVERSION_STRING_LONG   "1.0.0.1"
+#define RVERSION_STRING        "1.2.0.0"
+#define RVERSION_STRING_SHORT  "1.2.0"
+#define RVERSION_STRING_LONG   "1.2.0.0"
 #define RVERSION_STRING_DATE   __DATE__
 #define RVERSION_STRING_TIME   __TIME__
 #define RVERSION_STRING_BUILD  "e46122b"
@@ -21,7 +21,7 @@
 #define RVERSION_STRING_HOST   "daphne"
 #define RVERSION_STRING_USER   "doug"
 #define RVERSION_STRING_DOMAIN "goodall.com"
-#define RVERSION_STRING_IP     "47-143-222-184"
+#define RVERSION_STRING_IP     "192-164-4-17"
 #define RVERSION_STRING_MAC    "c8:7f:54:6a:6b:42"
 #define RVERSION_STRING_CPU    "x86_64"
 #define RVERSION_SIZE_MAX      (sizeof("255.255.255.255")+1)
