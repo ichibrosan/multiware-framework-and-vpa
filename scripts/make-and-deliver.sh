1005b378b1dc9697ed80fd2ddd8dce56ef4fa98c
Douglas <doug@goodall.com>
2025-03-09 07:31:59 -0700

    move headers into include folder, enhance CMakeFiles.txt and make and build scripts

--- scripts/make-and-deliver.sh
+++ scripts/make-and-deliver.sh
@@ -10,7 +10,7 @@ make
 cp -v *.cgi             ~/public_html/fw/cgi-bin/
 cp -v fw                ~/public_html/fw/bin/
 cp -v look              ~/public_html/fw/bin/
-cp -v libmwfw*.a        ~/public_html/fw/bin/
+cp -v libmwfw*.a        ~/public_html/fw/lib/
 rm -f                   ~/public_html/fw/bin/vpad
 cp -v vpad*             ~/public_html/fw/bin/
 cp -v xmlrpc*           ~/public_html/fw/bin/
