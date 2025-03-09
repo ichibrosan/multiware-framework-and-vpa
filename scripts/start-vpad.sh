a71bd0dc24b2d482ea211c66121dffb457893998
Douglas <doug@goodall.com>
2025-03-09 07:33:43 -0700

    enhance start-vpad.sh

--- scripts/start-vpad.sh
+++ scripts/start-vpad.sh
@@ -3,4 +3,4 @@
 # daphne.goodall.com:/home/devo/public_html/fw/scripts/inetd-netstat-redirect.sh 2025-02-22 #
 # Copyright (c) 2025 Douglas Wade Goodall. All Rights Reserved.                             #
 #############################################################################################
-/home/devo/public_html/fw/cmake-build-debug/vpad &
+/home/devo/public_html/fw/bin/vpad &
