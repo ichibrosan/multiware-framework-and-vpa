b4a0b0e07d76192d5731a0b638a9de2547df1ee5
Douglas Wade Goodall <doug@goodall.com>
2025-07-10 07:30:59 -0700

    enhanced documentation of vparpc_client.cpp & CVpaRpc.cpp

--- include/version.h
+++ include/version.h
@@ -7,10 +7,10 @@
 #define VERSION_H
 #define RMAJ    5
 #define RMIN    7
-#define RREV    5
-#define RBLD    5
-#define RSTRING "5.7.5.5"
-#define RSHORT  "5.7.5"
+#define RREV    9
+#define RBLD    21
+#define RSTRING "5.7.9.21"
+#define RSHORT  "5.7.9"
 #define RLONG   RSTRING
 #define RDATE   __DATE__
 #define RTIME   __TIME__
