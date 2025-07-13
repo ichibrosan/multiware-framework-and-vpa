161c6f5263e55e8c4af87d3b1a986975d8fc9195
Douglas Wade Goodall <doug@goodall.com>
2025-07-13 02:34:23 -0700

    add structures utility to observe rpc function packet lengths

--- look.cpp
+++ look.cpp
@@ -11,17 +11,17 @@
  **************************************************************/
 void sine()
 {
-    auto * pWin = new window();
+    auto* pWin = new window();
     gpSemiGr->cosmetics(
-        SRUL,  SRUR,  SRLL,    // Corner characters: ┌ ┐ └ ┘
-        SRLR,  SVSR,  SVSL,    // Right corner and separators
-        SH,    SV);             // Horizontal ─ and vertical │ lines
+        SRUL, SRUR, SRLL, // Corner characters: ┌ ┐ └ ┘
+        SRLR, SVSR, SVSL, // Right corner and separators
+        SH, SV); // Horizontal ─ and vertical │ lines
     char szVersion[64];
-    sprintf(szVersion,"Virtual Protocol Adapter Look Utility "
-                            "Ver %d.%d.%d.%d",RMAJ,RMIN,RREV,RBLD);
+    sprintf(szVersion, "Virtual Protocol Adapter Look Utility "
+            "Ver %d.%d.%d.%d",RMAJ,RMIN,RREV,RBLD);
     pWin->set_title(szVersion);
     std::string ssCopr = "  Copyright ";
-    ssCopr.append("(c)");  // Append copyright symbol for compatibility
+    ssCopr.append("(c)"); // Append copyright symbol for compatibility
     ssCopr.append(" 2025 Douglas Wade Goodall. All Rights Reserved.");
     pWin->add_row(ssCopr);
     pWin->render();
@@ -44,41 +44,43 @@ void shmvars()
     printf("num_tests_processed is %d\n",
            gpSh->m_pShMemng->num_tests_processed);
     printf("num_tests_skipped is %d\n",
-            gpSh->m_pShMemng->num_tests_skipped);
+           gpSh->m_pShMemng->num_tests_skipped);
     printf("num_tests_passed is %d\n",
-            gpSh->m_pShMemng->num_tests_passed);
+           gpSh->m_pShMemng->num_tests_passed);
     printf("num_tests_failed is %d\n\n",
-            gpSh->m_pShMemng->num_tests_failed);
+           gpSh->m_pShMemng->num_tests_failed);
     gpSh->m_pShMemng->num_tests_processed = 0;
     int iNumTests = 7;
     printf("tests_processed_bits are ");
-    gpOS->printBinary(gpSh->m_pShMemng->tests_processed_bits,iNumTests);
+    gpOS->printBinary(gpSh->m_pShMemng->tests_processed_bits, iNumTests);
     printf("\ntests_passed_bits    are ");
-    gpOS->printBinary(gpSh->m_pShMemng->tests_passed_bits,iNumTests);
+    gpOS->printBinary(gpSh->m_pShMemng->tests_passed_bits, iNumTests);
     printf("\ntests_failed_bits    are ");
-    gpOS->printBinary(gpSh->m_pShMemng->tests_failed_bits,iNumTests);
+    gpOS->printBinary(gpSh->m_pShMemng->tests_failed_bits, iNumTests);
     printf("\ntests_skipped_bits   are ");
-    gpOS->printBinary(gpSh->m_pShMemng->tests_skipped_bits,iNumTests);
+    gpOS->printBinary(gpSh->m_pShMemng->tests_skipped_bits, iNumTests);
 }
 
 void auth_users()
 {
     printf("Credentials of Authenticated Users:");
-    for (int iRow=3;iRow<CFG_MAX_USERS+ROW_DATA;iRow++) {
-        if (0 < gpSh->m_pShMemng->creds[iRow].iAuthHandle) {
+    for (int iRow = 3; iRow < CFG_MAX_USERS + ROW_DATA; iRow++)
+    {
+        if (0 < gpSh->m_pShMemng->creds[iRow].iAuthHandle)
+        {
             printf("\n-------------------------------------------\n");
-            printf(    "szAuthUserName  is %s\n",
-                gpSh->m_pShMemng->creds[iRow].szAuthUserName);
-            printf(    "szAuthFirstname is %s\n",
-                gpSh->m_pShMemng->creds[iRow].szAuthFirstName);
-            printf(    "szAuthLastName  is %s\n",
-                gpSh->m_pShMemng->creds[iRow].szAuthLastName);
-            printf(    "szAuthUUID      is %s\n",
-                gpSh->m_pShMemng->creds[iRow].szAuthUUID);
-            printf(    "iAuthHandle     is %d\n",
-                gpSh->m_pShMemng->creds[iRow].iAuthHandle);
-            printf(    "iHttpUserAgent  is %s\n",
-         gpSh->m_pShMemng->creds[iRow].szHttpUserAgent);
+            printf("szAuthUserName  is %s\n",
+                   gpSh->m_pShMemng->creds[iRow].szAuthUserName);
+            printf("szAuthFirstname is %s\n",
+                   gpSh->m_pShMemng->creds[iRow].szAuthFirstName);
+            printf("szAuthLastName  is %s\n",
+                   gpSh->m_pShMemng->creds[iRow].szAuthLastName);
+            printf("szAuthUUID      is %s\n",
+                   gpSh->m_pShMemng->creds[iRow].szAuthUUID);
+            printf("iAuthHandle     is %d\n",
+                   gpSh->m_pShMemng->creds[iRow].iAuthHandle);
+            printf("iHttpUserAgent  is %s\n",
+                   gpSh->m_pShMemng->creds[iRow].szHttpUserAgent);
         }
     }
 }
@@ -88,7 +90,8 @@ void configini()
     cfgini config("/home/doug/config.ini");
 
     // Create a new config or load existing
-    if (!config.load()) {
+    if (!config.load())
+    {
         config.createNew();
     }
 
@@ -114,19 +117,31 @@ void configini()
 
 void struct_diag()
 {
-    std::cout << "sizeof(vparpc_request_generic_t) is " << sizeof(vparpc_request_generic_t) << std::endl;
-    std::cout << "sizeof(vparpc_request_auth_t) is " << sizeof(vparpc_request_auth_t) << std::endl;
-    std::cout << "sizeof(vparpc_request_version_t) is " << sizeof(vparpc_request_version_t) << std::endl;
-    std::cout << "sizeof(vparpc_request_lookup_t) is " << sizeof(vparpc_request_lookup_t) << std::endl;
-    std::cout << "sizeof(vparpc_request_creds_t) is " << sizeof(vparpc_request_creds_t) << std::endl;
-    std::cout << "sizeof(vparpc_request_urls_t) is " << sizeof(vparpc_request_urls_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_generic_t) is " << sizeof(
+        vparpc_request_generic_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_auth_t) is " << sizeof(
+        vparpc_request_auth_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_version_t) is " << sizeof(
+        vparpc_request_version_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_lookup_t) is " << sizeof(
+        vparpc_request_lookup_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_creds_t) is " << sizeof(
+        vparpc_request_creds_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_urls_t) is " << sizeof(
+        vparpc_request_urls_t) << std::endl;
 
-    std::cout << "vparpc_request_generic_t padding is " << 1080 - sizeof(vparpc_request_generic_t) << std::endl;
-    std::cout << "vparpc_request_auth_t    padding is " << 1080 - sizeof(vparpc_request_auth_t) << std::endl;
-    std::cout << "vparpc_request_version_t padding is " << 1080 - sizeof(vparpc_request_version_t) << std::endl;
-    std::cout << "vparpc_request_lookup_t  padding is " << 1080 - sizeof(vparpc_request_lookup_t) << std::endl;
-    std::cout << "vparpc_request_creds_t   padding is " << 1080 - sizeof(vparpc_request_creds_t) << std::endl;
-    std::cout << "vparpc_request_urls_t    padding is " << 1080 - sizeof(vparpc_request_urls_t) << std::endl;
+    std::cout << "vparpc_request_generic_t padding is " << 1080 - sizeof(
+        vparpc_request_generic_t) << std::endl;
+    std::cout << "vparpc_request_auth_t    padding is " << 1080 - sizeof(
+        vparpc_request_auth_t) << std::endl;
+    std::cout << "vparpc_request_version_t padding is " << 1080 - sizeof(
+        vparpc_request_version_t) << std::endl;
+    std::cout << "vparpc_request_lookup_t  padding is " << 1080 - sizeof(
+        vparpc_request_lookup_t) << std::endl;
+    std::cout << "vparpc_request_creds_t   padding is " << 1080 - sizeof(
+        vparpc_request_creds_t) << std::endl;
+    std::cout << "vparpc_request_urls_t    padding is " << 1080 - sizeof(
+        vparpc_request_urls_t) << std::endl;
 }
 
 /*
@@ -168,75 +183,114 @@ void services_diag()
     // }
 
     // Check if a service exists
-    if (services.hasService("vpa")) {
+    if (services.hasService("vpa"))
+    {
         std::cout << "vpa service exists" << std::endl;
-    } else {
-        if (services.addService("vpa", 5164, "tcp", {"vpa-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa", 5164, "tcp", {"vpa-alias"}))
+        {
             std::cout << "Service added successfully - vpa" << std::endl;
         }
     }
 
-    if (services.hasService("vpa-disc")) {
+    if (services.hasService("vpa-disc"))
+    {
         std::cout << "vpa-disc service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-disc", 5164, "udp", {"vpa-disc-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa-disc", 5164, "udp", {"vpa-disc-alias"}))
+        {
             std::cout << "Service added successfully - vpa-disc" << std::endl;
         }
     }
 
 
-    if (services.hasService("vpad-start")) {
+    if (services.hasService("vpad-start"))
+    {
         std::cout << "vpa-start service exists" << std::endl;
-    } else {
-        if (services.addService("vpad-start", 65353, "tcp", {"vpa-start-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpad-start", 65353, "tcp",
+                                {"vpa-start-alias"}))
+        {
             std::cout << "Service added successfully - vpad-start" << std::endl;
         }
     }
 
-    if (services.hasService("vpa-netstat")) {
+    if (services.hasService("vpa-netstat"))
+    {
         std::cout << "vpa-netstat service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-netstat", 65354, "tcp", {"vpa-netstat-alias"})) {
-            std::cout << "Service added successfully - vpa-netstat" << std::endl;
+    }
+    else
+    {
+        if (services.addService("vpa-netstat", 65354, "tcp",
+                                {"vpa-netstat-alias"}))
+        {
+            std::cout << "Service added successfully - vpa-netstat" <<
+                std::endl;
         }
     }
 
-    if (services.hasService("vpa-ip")) {
+    if (services.hasService("vpa-ip"))
+    {
         std::cout << "vpa-ip service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-ip", 65355, "tcp", {"vpa-ip-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa-ip", 65355, "tcp", {"vpa-ip-alias"}))
+        {
             std::cout << "Service added successfully - vpa-ip" << std::endl;
         }
     }
 
-    if (services.hasService("vpa-curl")) {
+    if (services.hasService("vpa-curl"))
+    {
         std::cout << "vpa-curl service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-curl", 5164, "tcp", {"vpa-curl-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa-curl", 5164, "tcp", {"vpa-curl-alias"}))
+        {
             std::cout << "Service added successfully - vpa-curl" << std::endl;
         }
     }
 
-    if (services.hasService("vpa-http")) {
+    if (services.hasService("vpa-http"))
+    {
         std::cout << "vpa-http service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-http", 65357, "tcp", {"vpa-http-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa-http", 65357, "tcp", {"vpa-http-alias"}))
+        {
             std::cout << "Service added successfully - vpa-http" << std::endl;
         }
     }
 
-    if (services.hasService("vpa-https")) {
+    if (services.hasService("vpa-https"))
+    {
         std::cout << "vpa-https service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-https", 65358, "tcp", {"vpa-https-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa-https", 65358, "tcp", {"vpa-https-alias"}))
+        {
             std::cout << "Service added successfully - vpa-https" << std::endl;
         }
     }
 
-    if (services.hasService("vpa-ipcs")) {
+    if (services.hasService("vpa-ipcs"))
+    {
         std::cout << "vpa-ipcs service exists" << std::endl;
-    } else {
-        if (services.addService("vpa-ipcs", 65359, "tcp", {"vpa-ipcs-alias"})) {
+    }
+    else
+    {
+        if (services.addService("vpa-ipcs", 65359, "tcp", {"vpa-ipcs-alias"}))
+        {
             std::cout << "Service added successfully - vpa" << std::endl;
         }
     }
@@ -244,7 +298,8 @@ void services_diag()
 
     // Find services by pattern
     auto webServices = services.findServices("http");
-    for (const auto& service : webServices) {
+    for (const auto& service : webServices)
+    {
         std::cout << service.name << " on port " << service.port << std::endl;
     }
 }
@@ -252,64 +307,81 @@ void services_diag()
 int xinetdcfg_diag()
 {
     xinetcfg config;
-    
+
     // Test VPA configuration generation
     std::cout << "=== VPA Xinetd Configuration Test ===" << std::endl;
-    std::cout << "Number of services configured: " << config.getServiceCount() << std::endl;
-    
+    std::cout << "Number of services configured: " << config.getServiceCount()
+        << std::endl;
+
     // Display service status
     config.displayVpaServiceStatus();
-    
+
     // Generate and display configuration
     std::cout << "\n=== Generated VPA Configuration ===" << std::endl;
     std::string vpaConfig = config.generateVpaXinetdConfig();
     std::cout << vpaConfig << std::endl;
-    
+
     // Check permissions before attempting to save
-    if (!config.canModifyXinetd()) {
+    if (!config.canModifyXinetd())
+    {
         std::cout << "\n=== PERMISSION NOTICE ===" << std::endl;
-        std::cout << "Cannot modify /etc/xinetd.d/vpa - insufficient privileges." << std::endl;
+        std::cout <<
+            "Cannot modify /etc/xinetd.d/vpa - insufficient privileges." <<
+            std::endl;
         config.displaySudoInstructions();
-        std::cout << "\nConfiguration displayed above (dry-run mode)." << std::endl;
+        std::cout << "\nConfiguration displayed above (dry-run mode)." <<
+            std::endl;
         return 0;
     }
-    
+
     // Automatically save the configuration without prompting
     std::cout << "\nSaving configuration to /etc/xinetd.d/vpa..." << std::endl;
-    
+
     // Create backup first
     std::cout << "Creating backup of existing configuration..." << std::endl;
-    if (!config.createBackup()) {
+    if (!config.createBackup())
+    {
         std::cout << "WARNING: Could not create backup file." << std::endl;
     }
-    
+
     // Save the configuration
-    if (config.saveConfig()) {
-        std::cout << "SUCCESS: Configuration saved to /etc/xinetd.d/vpa" << std::endl;
-        
+    if (config.saveConfig())
+    {
+        std::cout << "SUCCESS: Configuration saved to /etc/xinetd.d/vpa" <<
+            std::endl;
+
         // Automatically reload xinetd
         std::cout << "Reloading xinetd service..." << std::endl;
-        if (config.reloadXinetd()) {
+        if (config.reloadXinetd())
+        {
             std::cout << "SUCCESS: xinetd reloaded successfully." << std::endl;
-        } else {
-            std::cout << "ERROR: Failed to reload xinetd. You may need to restart it manually." << std::endl;
         }
-    } else {
-        std::cout << "ERROR: Failed to save configuration to /etc/xinetd.d/vpa" << std::endl;
+        else
+        {
+            std::cout <<
+                "ERROR: Failed to reload xinetd. You may need to restart it manually."
+                << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "ERROR: Failed to save configuration to /etc/xinetd.d/vpa"
+            << std::endl;
         return 1;
     }
-    
+
     return 0;
 }
 
-int main() {
-    auto * pMwFw = new mwfw2(__FILE__,__FUNCTION__);
+int main()
+{
+    auto* pMwFw = new mwfw2(__FILE__, __FUNCTION__);
     sine();
     //shmvars();
     //auth_users();
     //configini();
-    //struct_diag();
+    struct_diag();
     //services_diag();
-    xinetdcfg_diag();
+    //xinetdcfg_diag();
     return EXIT_SUCCESS;
 }
---
+++ structures.cpp
@@ -0,0 +1,71 @@
+////////////////////////////////////////////////////////////////////
+// /home/devo/public_html/fw/structures.cpp 2025/07/13 02:32 dwg  //
+// Copyright (c) Douglas Wade Goodall. All Rights Reserved.       //
+////////////////////////////////////////////////////////////////////
+#include "mwfw2.h"
+#include "cfgini.h"
+#include "xinetdcfg.h"
+
+/**************************************************************
+ * Display Retro Header Information IBM 5150 Monochrome style *
+ **************************************************************/
+void sine()
+{
+    auto* pWin = new window();
+
+    gpSemiGr->cosmetics(
+        SRUL, SRUR, SRLL,
+        // Corner characters: ┌ ┐ └ ┘
+        SRLR, SVSR, SVSL,
+        // Right corner and separators
+        SH, SV);
+    // Horizontal ─ and vertical │ lines
+
+    char szVersion[64];
+    sprintf(szVersion, "Virtual Protocol Adapter Structures Utility "
+            "Ver %d.%d.%d.%d",RMAJ,RMIN,RREV,RBLD);
+    pWin->set_title(szVersion);
+    std::string ssCopr = "  Copyright ";
+    ssCopr.append("(c)"); // Append copyright symbol for compatibility
+    ssCopr.append(" 2025 Douglas Wade Goodall. All Rights Reserved.");
+    pWin->add_row(ssCopr);
+    pWin->render();
+}
+
+
+void struct_diag()
+{
+    std::cout << "sizeof(vparpc_request_generic_t) is " << sizeof(
+        vparpc_request_generic_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_auth_t) is " << sizeof(
+        vparpc_request_auth_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_version_t) is " << sizeof(
+        vparpc_request_version_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_lookup_t) is " << sizeof(
+        vparpc_request_lookup_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_creds_t) is " << sizeof(
+        vparpc_request_creds_t) << std::endl;
+    std::cout << "sizeof(vparpc_request_urls_t) is " << sizeof(
+        vparpc_request_urls_t) << std::endl;
+
+    std::cout << "vparpc_request_generic_t padding is " << 1080 - sizeof(
+        vparpc_request_generic_t) << std::endl;
+    std::cout << "vparpc_request_auth_t    padding is " << 1080 - sizeof(
+        vparpc_request_auth_t) << std::endl;
+    std::cout << "vparpc_request_version_t padding is " << 1080 - sizeof(
+        vparpc_request_version_t) << std::endl;
+    std::cout << "vparpc_request_lookup_t  padding is " << 1080 - sizeof(
+        vparpc_request_lookup_t) << std::endl;
+    std::cout << "vparpc_request_creds_t   padding is " << 1080 - sizeof(
+        vparpc_request_creds_t) << std::endl;
+    std::cout << "vparpc_request_urls_t    padding is " << 1080 - sizeof(
+        vparpc_request_urls_t) << std::endl;
+}
+
+int main()
+{
+    auto* pMwFw = new mwfw2(__FILE__, __FUNCTION__);
+    sine();
+    struct_diag();
+    return EXIT_SUCCESS;
+}
