a9698d68a972a78c24b85a1e5fc401a612174e80
Douglas Wade Goodall <doug@goodall.com>
2025-07-20 03:04:06 -0700

    change iPhone.cgi to use xterm with red background fullscreen

--- iPhone.cpp
+++ iPhone.cpp
@@ -10,23 +10,10 @@ int main()
 {
     auto pMwFw = std::make_unique<mwfw2>(__FILE__, __FUNCTION__);
     std::cout << "Content-type: text/html\n\n" << std::endl;
-    
-    // Set the DISPLAY environment variable
-    setenv("DISPLAY", ":0", 1);  // or ":0.0" depending on your setup
-    
-    //std::string command = "celluloid /var/www/html/2025-07-18-182734.webm";
-    //std::string command = "/usr/bin/firefox ";
-    std::string command = "/usr/bin/opera ";
 
-    //command.append("http://daphne/2025-07-18-182734.webm");
-
-    command.append("http://daphne/diane-needs-help.html");
-    //std::cout << command << std::endl;
-    
+    setenv("DISPLAY", ":0", 1);
+    std::string command = "xterm -fullscreen -bg \"#FF0000\" &";
     int result = system(command.c_str());
-    // if (result != 0) {
-    //     std::cerr << "Firefox failed to launch, exit code: " << result << std::endl;
-    // }
     std::cout << "Message sent";
 
     return 0;
