c82f5769895073a525f0b6f5c3083cf767879dbf
Douglas Wade Goodall <doug@goodall.com>
2025-11-17 13:27:33 -0800

    OPT and TIF csv parsing working ok

--- epstein.cpp
+++ epstein.cpp
@@ -80,16 +80,25 @@ int main(int argc, char** argv)
     sprintf(szTemp, "Config file: %s", ssCfgFQFS.c_str());
     pWin->add_row(szTemp);
 
-    std::string ssCSV = "20250822-opt.csv";
-    sprintf(szTemp, "CSV file: %s", ssCSV.c_str());
+    std::string ssOptCSV = "20250822-opt.csv";
+    sprintf(szTemp, "OPT file: %s", ssOptCSV.c_str());
+    pWin->add_row(szTemp);
+    readCsv * pCsvOpt = new readCsv(ssOptCSV);
+    pCsvOpt->parseData();
+    int iOptLines = pCsvOpt->getLines();
+    sprintf(szTemp, "OPT file has %d lines", iOptLines);
     pWin->add_row(szTemp);
 
-    readCsv * pCsv = new readCsv(ssCSV);
-    pCsv->parseData();
-
-    int iLines = pCsv->getLines();
-    sprintf(szTemp, "CSV file has %d lines", iLines);
+    std::string ssTifCSV = "20250822-tif.csv";
+    sprintf(szTemp, "TIF file: %s", ssTifCSV.c_str());
     pWin->add_row(szTemp);
+    readCsv * pCsvTif = new readCsv(ssTifCSV);
+    pCsvTif->parseData();
+    int iTifLines = pCsvTif->getLines();
+    sprintf(szTemp, "TIF file has %d lines", iTifLines);
+    pWin->add_row(szTemp);
+
+
 
     FILE * fd = fopen("/home/doug/public_html/fw/html/datafile.html","w");
     fprintf(fd,"<!DOCTYPE html>\n");
@@ -100,47 +109,21 @@ int main(int argc, char** argv)
     fprintf(fd,"</head>\n");
     fprintf(fd,"<body>\n");
 
-    iLines = 10;
-    for (int index=0; index<iLines; index++)
+    iOptLines = 10;
+    for (int index=0; index<iOptLines; index++)
     {
-        //int field = 0;
-
-        //sprintf(szTemp,"field %d: %s",field,pCsv->getData(index,field).c_str());
-        //pWin->add_row(szTemp);
-
-        //field++;
-        //sprintf(szTemp,"field %d: %s",field,pCsv->getData(index,field).c_str());
-        //pWin->add_row(szTemp);
-
-        //field++;
-        //sprintf(szTemp,"field %d: %s",field,pCsv->getData(index,field).c_str());
-        //pWin->add_row(szTemp);
-
 
         std::string ssImgFQFS = "http://localhost/~doug/epstein/DOJ_Epstein_Files_First_Production/";
-        std::string ssField2  = pCsv->getData(index,2);
+        std::string ssField2  = pCsvOpt->getData(index,2);
 
         std::string ssImgFrag = ssField2.substr(2,ssField2.length());
-        //sprintf(szTemp,"frag: %s",ssImgFrag.c_str());
-        //pWin->add_row(szTemp);
 
         ssImgFQFS.append(ssImgFrag);
         sprintf(szTemp,"ssImgFQFS: %s",ssImgFQFS.c_str());
         pWin->add_row(szTemp);
 
-        //fprintf(fd,"%d: %s\n",index,ssImgFQFS.c_str());
-
-        //fprintf(fd,"<p><img src=\"%s\" width=\"100\" height=\"100\">\n",ssImgFQFS.c_str());
         fprintf(fd,"<p><img src=\"%s\" width=\"1024\" height=\"768\">\n",ssImgFQFS.c_str());
 
-
-        // char szCommand[FILENAME_MAX];
-        // sprintf(szCommand,"firefox http://localhost/~doug/epstein/DOJ_Epstein_First_Production/"
-        // "%s",ssImgFQFS.c_str());
-        // pWin->add_row(szCommand);
-
-
-        //system(szCommand);
     }
     pWin->render();
     fprintf(fd,"</body>\n");
